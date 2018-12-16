9420c6891c8731d467cfffc6c9f9d5f749c2bcc8
konstantin <konstantin.re@gmail.com>
2018-12-16 23:22:27 +0300

    WIP watcher and importer

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import mainConfig from "./config/main";
 import {User, Product} from "./models"
+import Importer from "./lib/importer"
 
-console.log(mainConfig.name);
+//console.log(mainConfig.name);
 
-const user = new User;
-const product = new Product();
+// const user = new User;
+// const product = new Product();
+
+const path = './data/sample.csv';
+const result = Importer.import(path);
+console.log(result);
