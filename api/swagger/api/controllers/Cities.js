e2e1838d7f36cf0d61598df49d944724ecc98aca
Konstantin <konstantin.re@gmail.com>
2019-04-12 14:39:34 +0300

    aditional methods

--- api/swagger/api/controllers/Cities.js
+++ api/swagger/api/controllers/Cities.js
@@ -2,12 +2,66 @@ import City from "../../../models/mongoose/city"
 
 
 module.exports = {
-    index: index
+    indexCity,
+    createCity,
+    updateCity,
+    deleteCity
 };
 
-function index(req, res) {
+function indexCity(req, res) {
     City.find({}, (err, cities) => {
         if (err) return res.send(err);
         return res.send(cities);
+    });
+}
+
+function createCity(req, res) {
+    if (!req.body || !req.body.name) {
+        return res.send("Please specify name");
+    }
+    City.create(
+        {
+            name: req.body.name,
+            country: req.body.country,
+            capital: req.body.capital,
+            location: {
+                lat: req.body.lat,
+                long: req.body.long
+            }
+        },
+        (err, city) => {
+            if (err) return res.send(err);
+            return res.send(city);
+        }
+    );
+}
+
+function updateCity(req, res) {
+    if (!req.body || !req.body.name) {
+        return res.send("Please specify name");
+    }
+    City.findOneAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            country: req.body.country,
+            capital: req.body.capital,
+            location: {
+                lat: req.body.lat,
+                long: req.body.long
+            }
+        },
+        { new: true },
+        (err, city) => {
+            if (err) return res.send(err);
+            return res.send(city);
+        }
+    );
+}
+
+function deleteCity(req, res) {
+    City.deleteOne({ _id: req.params.id }, (err, city) => {
+        if (err) return res.send(err);
+        return res.send(city);
     });
 }
